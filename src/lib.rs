045ec221cc82763bf2bc5c14cb472bfbdce69b33
Daniel Avishai Stutman <git@stutman.tech>
2022-05-11 22:48:13 +0200

    Add `single_sample` test
    This test just runs a single state sample through the pipeline to check
    that nothing explodes.

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,45 @@ mod util;
 
 #[cfg(test)]
 mod tests {
+    #[cfg(test)]
+    extern crate std;
+
+    use crate::regulator::{MotorProperties, PhaseCurrents, RegulatorConfig, State};
+
+    fn init_logs() {
+        let _ = env_logger::builder()
+            .is_test(true)
+            .filter_level(log::LevelFilter::Debug)
+            .try_init()
+            .unwrap();
+    }
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn single_sample() {
+        init_logs();
+
+        let mut regulator = RegulatorConfig::new(MotorProperties {
+            inertia: 1E-5,
+            pole_pairs: 7,
+            phase_resistance: 22.,
+            torque_constant: 5E-2,
+        })
+        .realize();
+
+        let state = State {
+            currents: PhaseCurrents {
+                u: 1.,
+                v: 1.,
+                w: -2.,
+            },
+            position: -1.5,
+            velocity: 0.,
+            timestamp_us: 1000,
+        };
+        regulator.update(state);
+
+        let duties = crate::modulation::space_vector_modulation(regulator.output());
+
+        log::debug!("Calculated phase duties: {:?}", duties)
     }
 }
